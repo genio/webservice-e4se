0536ecfdc5536f48e8563dbadbc3b64ca9932e71
Chase Whitener <chase.whitener@infotechfl.com>
2015-10-14 14:44:44 -0400

    Updated lots of documentation, changed to moo, changed a link at the request of Epicor

--- lib/WebService/E4SE.pm
+++ lib/WebService/E4SE.pm
@@ -1,14 +1,11 @@
 package WebService::E4SE;
 
-use 5.006;
-use Moose;
-use MooseX::Types::Moose qw/Bool HashRef/;
-use Carp;
-
+use Moo;
 use Authen::NTLM 1.09;
 use LWP::UserAgent 6.02;
 use HTTP::Headers;
 use HTTP::Request;
+use Scalar::Util ();
 use URI 1.60;
 use XML::Compile::SOAP11 2.38;
 use XML::Compile::SOAP11::Client;
@@ -16,97 +13,38 @@ use XML::Compile::WSDL11;
 use XML::Compile::Transport::SOAPHTTP;
 use XML::LibXML;
 
-use namespace::autoclean;
-
-=head1 NAME
-
-WebService::E4SE - Communicate with the various Epicor E4SE web services.
-
-=head1 VERSION
-
-Version 0.021
-
-=cut
+use Carp ();
+use strictures 2;
+use namespace::clean;
+use v5.10;
 
 our $AUTHORITY = 'cpan:CAPOEIRAB';
-our $VERSION = '0.021';
-#$Carp::Verbose = 1;
-has useragent => (
-	is => 'ro',
-	isa => 'LWP::UserAgent',
-	required => 1,
-	init_arg => undef,
-	default => sub {
-		my $lwp = LWP::UserAgent->new( keep_alive=>1 )
-	}
+our $VERSION = '0.03';
+
+has _wsdls => (
+	is => 'lazy',
+	isa => sub { die "Not a HashRef" unless ref($_[0]) eq 'HASH' },
+	default => sub { {} },
 );
 
-has timeout => (
-	is => 'rw',
-	isa => 'Int',
-	required => 1,
-	default => 30,
-);
-
-has username => (
-	is => 'rw',
-	isa => 'Str',
-	required => 1,
-	default => '',
-);
-
-has password => (
-	is => 'rw',
-	isa => 'Str',
-	required => 1,
-	default => '',
-);
-
-has realm => (
-	is => 'rw',
-	isa => 'Str',
-	required => 1,
-	default => '',
-);
-
-has site => (
-	is => 'rw',
-	isa => 'Str',
-	required => 1,
-	default => 'epicor:80',
+has _ua => (
+	is => 'lazy',
+	isa => sub { die "Not an LWP::UserAgent" unless Scalar::Util::blessed($_[0]) && $_[0]->isa('LWP::UserAgent') },
+	default => sub { LWP::UserAgent->new( keep_alive=>1 ) },
 );
 
 has base_url => (
 	is => 'rw',
-	isa => 'URI',
+	isa => sub {die "Not an URI" unless Scalar::Util::blessed($_[0]) && $_[0]->isa('URI')},
 	required => 1,
 	default => sub {
 		URI->new('http://epicor/e4se')
 	}
 );
 
-has wsdl => (
-	is => 'rw',
-	isa => 'HashRef[XML::Compile::WSDL11]',
-	required => 0,
-	init_arg => undef,
-	lazy => 1,
-	default => sub { {} },
-);
-
-has force_wsdl_reload => (
-	is => 'rw',
-	isa => 'Bool',
-	required => 1,
-	default => 0
-);
-
 has files => (
-	is => 'ro',
-	isa => 'ArrayRef[Str]',
-	required => 1,
-	lazy => 1,
-	init_arg => undef,
+	is => 'lazy',
+	isa => sub { die 'Should be an array reference' unless $_[0] && ref($_[0]) eq 'ARRAY' },
 	default => sub {[
 		'ActionCall.asmx',
 		'Attachment.asmx',
@@ -251,6 +189,37 @@ has files => (
 	]},
 );
 
+has force_wsdl_reload => (
+	is => 'rw',
+	coerce => sub { return 0 unless $_[0]; return 1 if ref($_[0]); (lc($_[0]) eq 'false') ? 0 : 1},
+	required => 1,
+	default => 0
+);
+
+has password => (
+	is => 'rw',
+	required => 1,
+	default => '',
+);
+
+has realm => (
+	is => 'rw',
+	required => 1,
+	default => '',
+);
+
+has site => (
+	is => 'rw',
+	required => 1,
+	default => 'epicor:80',
+);
+
+has username => (
+	is => 'rw',
+	required => 1,
+	default => '',
+);
+
 sub _get_port {
 	my ( $self, $file ) = @_;
 	return "WSSoap" unless defined($file) and length($file);
@@ -267,182 +236,32 @@ sub _valid_file {
 
 sub _wsdl {
 	my ( $self, $file ) = @_;
-	my $wsdl = $self->wsdl();
+	my $wsdl = $self->_wsdls();
 	#if our wsdl is already setup, let's just return
 	return 1 if ( exists($wsdl->{$file}) && defined($wsdl->{$file}) );
 
 	#wsdl doesn't exist.  let's setup the user agent for our transport and move along
-	$self->useragent->credentials( $self->site, $self->realm, $self->username, $self->password );
-	$self->useragent->timeout($self->timeout);
+	$self->_ua->credentials( $self->site, $self->realm, $self->username, $self->password );
 
-	my $res = $self->useragent->get($self->base_url . '/'. $file . '?WSDL' );
-	unless ( $res->is_success ) {
-		Carp::carp( "Unable to setup WSDL: ".$res->status_line() );
-		return 0;
-	}
+	my $res = $self->_ua->get($self->base_url . '/'. $file . '?WSDL' );
+	Carp::croak('Unable to setup WSDL: '.$res->status_lin()) unless $res->is_success;
+
 	$wsdl->{$file} = XML::Compile::WSDL11->new( $res->decoded_content );
-	unless ( $wsdl->{$file} ) {
-		Carp::carp( "Unable to create new XML::Compile::WSDL11 object" );
-		return 0;
-	}
+	Carp::croak( "Unable to create new XML::Compile::WSDL11 object" ) unless $wsdl->{$file};
+
 	my $trans = XML::Compile::Transport::SOAPHTTP->new(
-		user_agent=> $self->useragent,
+		user_agent=> $self->_ua,
 		address => $self->base_url.'/'. $file,
 	);
-	unless ( $trans ) {
-		Carp::carp( "Unable to create new XML::Compile::Transport::SOAPHTTP object" );
-		return 0;
-	}
+	Carp::carp( "Unable to create new XML::Compile::Transport::SOAPHTTP object" ) unless $trans;
+
 	$wsdl->{$file}->compileCalls(
 		port => $self->_get_port($file),
 		transport => $trans,
 	);
+	$self->_wsdls($wsdl);
 	return 1;
 }
-
-=head1 SYNOPSIS
-
-Epicor's L<http://www.epicor.com/Products/Pages/E4SE.aspx> E4SE software
-uses a clunky, IE-only interface (just try to fill in your timesheet with
-a non-Windows machine!  I dare ya!).
-
-Each action on the software calls a SOAP-based web service API method.
-However, the APIs are not neatly packaged in one place (a la Salesforce, etc.).
-Also, there aren't any sessions. So, each call to the API must send username
-and password credentials all over again (using NTLM).
-
-There are more than 100 web service files you could work with (.asmx extensions)
-each having their own set of methods.  Some of these are grouped somewhat logically,
-some are not.  On your installation of E4SE, you can get a listing of method calls
-available by visiting one of those files directly (L<http://epicor/e4se/Resource.asmx>
-for example).
-
-The purpose of this module is to somewhat abstract out the tedium of dealing with
-all of this stuff.
-
-The module will grab the WSDL from the file you're trying to deal with.  It will make
-use of that WSDL with L<XML::Compile::WSDL11>.  You can force a reload of the WSDL at any
-time.  So, we build the L<XML::Compile::WSDL11> object and hold onto it for any further
-calls to that file.  These are generated by the calls you make, so hopefully we don't
-kill you with too many objects.  You can work directly with the new L<XML::Compile::WSDL11>
-object if you like, or use the abstracted out methods listed below.
-
-For transportation, we're using L<XML::Compile::Transport::SOAPHTTP> using L<LWP::UserAgent> with L<Authen::NTLM>.
-
-Here's some sample code:
-
-  use WebService::E4SE;
-
-  # create a new object
-  my $ws = WebService::E4SE->new(
-    username => 'AD\username',                  # NTLM authentication
-    password => 'A password',                   # NTLM authentication
-    realm => '',                                # LWP::UserAgent and Authen::NTLM
-    site => 'epicor:80',                        # LWP::UserAgent and Authen::NTLM
-    base_url => URL->new('http://epicor/e4se'), # LWP::UserAgent and Authen::NTLM
-    timeout => 30,                              # LWP::UserAgent
-  );
-
-  # get an array ref of web service APIs to communicate with
-  my $res = $ws->files();
-  say Dumper $res;
-
-  # returns a list of method names for the file you wanted to know about.
-  my @operations = $ws->operations('Resource.asmx');
-  say Dumper @operations;
-
-  # call a method and pass some named parameters to it
-  my ($res,$trace) = $ws->call('Resource.asmx','GetResourceForUserID', userID=>'someuser');
-
-  # give me the XML::Compile::WSDL11 object
-  my $wsdl = $ws->get_object('Resource.asmx'); #returns the usable XML::Compile::WSDL11 object
-
-=head1 METHODS
-
-=head2 new( %params )
-
-The constructor will give you a new WebService::E4SE object.  The parameters passed to it are all listed below.  They have defaults and since this is a L<Moose> object, you can set each one individually as well by using the parameter name as a method call.
-
-=over
-
-=item parameters
-
-=over
-
-=item username
-
-Default is an empty string.  Usually, you need to prefix this with the domain your E4SE installation is using. 'AD\myusername'
-
-=item password
-
-Default is an empty string.  This will be your domain password.  No attempt to hide this is made as E4SE cannot function over SSL anyway, why should I bother trying to make you feel secure when you're not.
-
-=item realm
-
-Default is an empty string.  Again, this is for the L<Authen::NTLM> module and can generally be left blank.
-
-=item site
-
-Default is 'epicor:80'.  This is for the L<Authen::NTLM> module.  Set this accordingly
-
-=item base_url
-
-Type is L<URL>. Default is URL->new('http://epicor/e4se').  This should be the base installation path to your E4SE installation.
-
-=item timeout
-
-Default is 30.  This is passed to L<LWP::UserAgent> to handle how long you want to wait on responses.
-
-=back
-
-=back
-
-=head2 files()
-
-This method will return a reference to an array of file names that this web service has knowledge of for an E4SE installation.
-
-  my $files = $ws->files;  # 'ActionCall.asmx', 'Company.asmx', 'Resource.asmx', etc....
-
-=head2 call( $file, $function, %parameters )
-
-This method will call an API method for the file you want.  It will return 0 and warn on errors outside of L<XML::Compile::WSDL11>'s knowledge, otherwise it's just a little wrapper around L<XML::Compile::WSDL11>->call();
-
-Another way to do this would be $ws->get_object('Reource.asmx')->call( $function, %params );
-
-=over
-
-=item $file
-
-This is the string name of the file (also the WSDL11 object) you wish to deal with.  'Resource.asmx' for example.
-
-=item $function
-
-This is the string name of the function you want to call in the file's API.  'GetResourceForUserID' for xample.
-
-=item %parameters
-
-This is a hash of name => value pairs to be passed in for the function call.  This is XML escaped properly by L<XML::Compile::WSDL11>.
-
-=back
-
-=head2 get_object( $file )
-
-This method will return an L<XML::Compile::WSDL11> object for the string file name you supply.  This handles going to the file's WSDL URL,
-grabbing that URL with L<LWP::UserAgent> and L<Authen::NTLM>, and using that WSDL response to setup a new L<XML::Compile::WSDL11> object.  Note
-that if you have previously setup a L<XML::Compile::WSDL> object for that file name, it will just return that object rather than going to the
-server and requesting a new WSDL.
-
-=head2 force_wsdl_reload(0)
-
-This property is defaulted to false (0).  If set to true, the next call to a method that would require a L<XML::Compile::WSDL11> object will
-go out to the server and re-grab the WSDL and re-setup that WSDL object no matter if we have already generated it or not.  The property will
-be reset to false directly after the next WSDL object setup.
-
-=head2 operations( $file )
-
-This method will return a list of  L<XML::Compile::SOAP::Operation> objects that are available for the given file.
-
-=cut
 
 sub call {
 	my ( $self, $file, $function, %parameters ) = @_;
@@ -493,9 +312,173 @@ sub operations {
 	for my $op ( @ops ) {
 		push @ret, $op->name;
 	}
-	return @ret if wantarray;
 	return \@ret;
 }
+
+1; # End of WebService::E4SE
+
+=encoding utf8
+
+=head1 NAME
+
+WebService::E4SE - Communicate with the various Epicor E4SE web services.
+
+=head1 SYNOPSIS
+
+	use WebService::E4SE;
+
+	# create a new object
+	my $ws = WebService::E4SE->new(
+		username => 'AD\username',                  # NTLM authentication
+		password => 'A password',                   # NTLM authentication
+		realm => '',                                # LWP::UserAgent and Authen::NTLM
+		site => 'epicor:80',                        # LWP::UserAgent and Authen::NTLM
+		base_url => URL->new('http://epicor/e4se'), # LWP::UserAgent and Authen::NTLM
+		timeout => 30,                              # LWP::UserAgent
+	);
+
+	# get an array ref of web service APIs to communicate with
+	my $res = $ws->files();
+	say Dumper $res;
+
+	# returns a list of method names for the file you wanted to know about.
+	my @operations = $ws->operations('Resource.asmx');
+	say Dumper @operations;
+
+	# call a method and pass some named parameters to it
+	my ($res,$trace) = $ws->call('Resource.asmx','GetResourceForUserID', userID=>'someuser');
+
+	# give me the XML::Compile::WSDL11 object
+	my $wsdl = $ws->get_object('Resource.asmx'); #returns the usable XML::Compile::WSDL11 object
+
+=head1 DESCRIPTION
+
+L<WebService::E4SE> allows us to connect to
+L<Epicor's E4SE|http://www.epicor.com/products/e4se.aspx> SOAP-based APIs
+service to access our data or put in our timesheet.
+
+Each action on the software calls a SOAP-based web service API method. Each API
+call is authenticated via NTLM.
+
+There are more than 100 web service files you could work with (.asmx
+extensions) each having their own set of methods. On your installation of E4SE,
+you can get a listing of method calls available by visiting one of those files
+directly (C<http://your_epicor_server/e4se/Resource.asmx> for example).
+
+The module will grab the WSDL from the file you're trying to deal with.  It will make
+use of that WSDL with L<XML::Compile::WSDL11>.  You can force a reload of the WSDL at any
+time.  So, we build the L<XML::Compile::WSDL11> object and hold onto it for any further
+calls to that file.  These are generated by the calls you make, so hopefully we don't
+kill you with too many objects.  You can work directly with the new L<XML::Compile::WSDL11>
+object if you like, or use the abstracted out methods listed below.
+
+For transportation, we're using L<XML::Compile::Transport::SOAPHTTP> using
+L<LWP::UserAgent> with L<Authen::NTLM>.
+
+=head1 ATTRIBUTES
+
+L<WebService::E4SE> makes the following attributes available:
+
+=head2 base_url
+
+	my $url = $ws->base_url;
+	$url = $ws->base_url(URI->new('http://epicor/e4se'));
+
+This should be the base L<URL|URI> for your E4SE installation.
+
+=head2 files
+
+	my $files = $ws->files;
+	$files = $ws->files(['file1.asmx', 'file2.asmx']);
+	say join ', ', @$files;
+
+This is reference to an array of file names that this web service has
+knowledge of for an E4SE installation. If your installation has some services
+that we're missing, you can inject them here. This will clobber, not
+merge/append.
+
+=head2 force_wsdl_reload
+
+	my $force = $ws->force_wsdl_reload;
+	$force = $ws->force_wsdl_reload(1);
+
+This attribute is defaulted to false (0).  If set to true, the next call to a
+method that would require a L<XML::Compile::WSDL11> object will go out to the
+server and re-grab the WSDL and re-setup that WSDL object no matter if we have
+already generated it or not. The attribute will be reset to false (0) directly
+after the next WSDL object setup.
+
+=head2 password
+
+	my $pass = $ws->password;
+	$pass = $ws->password('foobarbaz');
+
+This will be your domain password.  No attempt to hide this is made.
+
+=head2 realm
+
+	my $realm = $ws->realm;
+	$realm = $ws->realm('MyADRealm');
+
+Default is an empty string.  This is for the L<Authen::NTLM> module and can generally be left blank.
+
+=head2 site
+
+	my $site = $ws->site;
+	$site = $ws->site('epicor:80');
+
+This is for the L<Authen::NTLM> module.  Set this accordingly.
+
+=head2 username
+
+	my $user = $ws->username;
+	$user = $ws->username('AD\myusername');
+
+Usually, you need to prefix this with the domain your E4SE installation is using.
+
+=head1 METHODS
+
+L<WebService::E4SE> makes the following methods available:
+
+=head2 call
+
+	use Try::Tiny;
+	try {
+		my ( $res, $trace) = $ws->call('Resource.asmx', 'GetResourceForUserID', %parameters );
+		say Dumper $res;
+	}
+	catch {
+		warn "An error happened: $_";
+		exit(1);
+	}
+
+This method will call an API method for the file you want.  It will die on
+errors outside of L<XML::Compile::WSDL11>'s knowledge, otherwise
+it's just a little wrapper around L<XML::Compile::WSDL11>->call();
+
+Another way to do this would be
+
+	$ws->get_object('Reource.asmx')->call( 'GetResourceForUserID', %params );
+
+=head2 get_object
+
+	my $wsdl = $ws->get_object('Resource.asmx');
+
+This method will return an L<XML::Compile::WSDL11> object for the file name
+you supply.  This handles going to the file's WSDL URL, grabbing that URL
+with L<LWP::UserAgent> and L<Authen::NTLM>, and using that WSDL response to
+setup a new L<XML::Compile::WSDL11> object.
+
+Note that if you have previously setup a L<XML::Compile::WSDL> object for that
+file name, it will just return that object rather than going to the server and
+requesting a new WSDL.
+
+=head2 operations
+
+	my $available_operations = $ws->operations( $file );
+
+This method will return a list of  L<XML::Compile::SOAP::Operation> objects
+that are available for the given file.
 
 =head1 AUTHOR
 
@@ -504,7 +487,7 @@ Chase Whitener << <cwhitener at gmail.com> >>
 =head1 BUGS
 
 Please report any bugs or feature requests on GitHub L<https://github.com/genio/webservice-e4se/issues>.
-I appreciate any and all criticism, bug reports, enhancements, or fixes.
+We appreciate any and all criticism, bug reports, enhancements, or fixes.
 
 =head1 SUPPORT
 
@@ -535,51 +518,12 @@ L<http://search.cpan.org/dist/WebService-E4SE/>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
-
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013 Chase Whitener.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+Copyright 2015
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
 
 =cut
-
-1; # End of WebService::E4SE
--- t/00-load.t
+++ t/00-load.t
@@ -1,28 +1,29 @@
 use strict;
 use warnings FATAL => 'all';
-use Test::More tests => 21;
+use Test::More;
 use URI;
+use Try::Tiny;
+use v5.10;
 
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
 BEGIN {
-	use_ok( 'WebService::E4SE' ) || print "Bail out!\n";
+	use_ok( 'WebService::E4SE' ) || BAIL_OUT("Can't use WebService::E4SE");
 }
 
 my $ws = WebService::E4SE->new();
-can_ok(
-	'WebService::E4SE',
-	qw(useragent timeout username password realm site base_url wsdl force_wsdl_reload files call get_object operations)
-);
+can_ok('WebService::E4SE', (
+	qw(_wsdls _ua base_url files force_wsdl_reload password realm site username), # attributes,
+	qw(_get_port _valid_file _wsdl call get_object operations), # methods
+));
+
 isa_ok($ws,'WebService::E4SE');
-isa_ok($ws->useragent(),'LWP::UserAgent');
+isa_ok($ws->_ua(),'LWP::UserAgent');
 ok($ws->username eq '');
 ok($ws->username('foo') eq 'foo');
 ok($ws->password eq '');
 ok($ws->password('foo') eq 'foo');
-ok($ws->timeout == 30);
-ok($ws->timeout(1) == 1);
 ok($ws->realm eq '');
 ok($ws->realm('foo') eq 'foo');
 ok($ws->site eq 'epicor:80');
@@ -30,10 +31,10 @@ ok($ws->site('foo') eq 'foo');
 isa_ok($ws->base_url(),'URI');
 ok($ws->base_url eq 'http://epicor/e4se');
 ok($ws->base_url(URI->new('http://foo.com')) eq 'http://foo.com');
-isa_ok($ws->wsdl, 'HASH');
+isa_ok($ws->_wsdls, 'HASH');
 ok($ws->force_wsdl_reload eq 0);
 ok($ws->force_wsdl_reload(1) eq 1);
 isa_ok($ws->files, 'ARRAY');
 
 
-#diag( "Testing WebService::E4SE $WebService::E4SE::VERSION, Perl $], $^X" );
+done_testing();
